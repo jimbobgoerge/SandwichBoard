267c05a407ee4b3532f667de722b11036a370d78
Shane Urban <surbanbusiness@gmail.com>
2019-09-12 19:59:22 -0600

    Got modals working on every cell

--- routes.py
+++ routes.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, render_template
+from flask import *
 import html
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def view_list():
-  return render_template("index.html")
+  user_file = ""
+  with open("sandwich_info.json", "r") as item:
+      user_file = item.read().rstrip("\n\r")
+      print(type(user_file))
+  return render_template("index.html", user_file=user_file)
 
 if __name__ == "__main__":
   app.run()
